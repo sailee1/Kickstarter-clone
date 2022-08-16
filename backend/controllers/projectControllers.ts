68c58c1eb1864bbcbd96db4c47006e9e9fb8cbb0
sailee1 <saiclee1986@googlemail.com>
2022-08-16 11:18:21 +0100

    Sanitized project

--- backend/controllers/projectControllers.ts
+++ backend/controllers/projectControllers.ts
@@ -1,98 +1,57 @@
-import {Response, Request} from 'express'
-const mongoose = require('mongoose');
-const asyncHandler = require('express-async-handler')
+import { Response, Request } from 'express';
+const asyncHandler = require('express-async-handler');
+import { createProject, getProjectById, getProjects, deleteProject, updateProject } from '../services/projectServices';
 
-const Project = require('../models/projectModels')
 
-//@desc Get a project by id
-//@route GET /api/projects/:id
-//@access Public
+
 //@desc Get all projects
 //@route GET /api/projects
 //@access Public
-const getProjects = asyncHandler(async (req: Request, res: Response) => {
-    const projects = await Project.find();
+const getProjectsHandler = asyncHandler(async (req: Request, res: Response) => {
+    const projects = await getProjects()
+   
     res.status(200).json(projects);
 });
-
 
 //@desc Create a new project
 //@route POST /api/projects
 //@access Private
-const createProject = asyncHandler(async(req: Request, res: Response)=> {
-    if (!req.body.title) {
-        res.status(400);
-        throw new Error('Title is required');
-    }
-    const project = await Project.create(req.body);
-    if (!project) {
-        res.status(400);
-        throw new Error('Project not created');
-    }
-    res.status(201).json(project);
-})
+const createProjectHandler = asyncHandler(async (req: Request, res: Response) => {
+    const createdProject = await createProject(req.body)
+    res.status(201).json(createdProject);
+});
 
 //@desc Get a project by id
 //@route GET /api/projects/:id
 //@access Public
-const getProject = asyncHandler(async (req: Request, res: Response) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        res.status(400);
-        throw new Error(`${req.params.id} is not a valid id`);
-    }
-    const project = await Project.findById(req.params.id);
-    if (!project) {
-        res.status(404);
-        throw new Error('Project not found');
-    }
+const getProjectHandler = asyncHandler(async (req: Request, res: Response) => {
+    const project = await getProjectById(req.params.id)
     res.status(200).json(project);
 });
-
-
-const updateProject = asyncHandler(async(req: Request, res: Response) => {
-    if (!req.body.title) {
-        res.status(400);
-        throw new Error('Title is required');
-    }
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        res.status(400);
-        throw new Error(`${req.params.id} is not a valid id`);
-    }
-    const project = await Project.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-    });
-    if (!project) {
-        res.status(404);
-        throw new Error('Project not found');
-    }
-    res.json(project);
-})
 
 //@desc Delete a project by id
 //@route DELETE /api/projects/:id
 //@access Private
-
-const deleteProject = asyncHandler(async(req: Request, res: Response) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        res.status(400);
-        throw new Error(`${req.params.id} is not a valid id`);
-    }
-    const project = await Project.findByIdAndDelete(req.params.id);
-    if (!project) {
-        res.status(404);
-        throw new Error('Project not found');
-    }
+const deleteProjectHandler = asyncHandler(async (req: Request, res: Response) => {
+    await deleteProject(req.params.id)
+    
     res.status(200).json({
         message: `Project ${req.params.id} deleted`,
-        project: project,
     });
-})
+});
 
+//@desc Update a project by id
+//@route PUT /api/projects/:id
+//@access Private
+const updateProjectHandler = asyncHandler(async (req: Request, res: Response) => {
+    const project = await updateProject(req.params.id, req.body)
+    res.json(project);
+});
 
-module.exports ={
-    getProjects,
-    createProject,
-    getProject,
-    updateProject,
-    deleteProject
-}
+module.exports = {
+    getProjectsHandler,
+    createProjectHandler,
+    getProjectHandler,
+    deleteProjectHandler,
+    updateProjectHandler,
+};
